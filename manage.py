45c615f5dea16eccecf33769244621caf3fb33c1
Charlie Mills <charlie.mills@just-eat.com>
2015-04-02 22:21:17 +0100

    Remove method to create spatialitedb
    No Longer required in django 1.8

--- manage.py
+++ manage.py
@@ -3,13 +3,7 @@ import os
 import sys
 import subprocess
 
-
-def create_spatialite_db():
-    if not os.path.isfile("db.sqlite3"):
-        subprocess.call(['spatialite', 'db.sqlite3', 'SELECT InitSpatialMetaData();'])
-
 if __name__ == "__main__":
-    create_spatialite_db()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TheAuditTrail.settings")
 
     from django.core.management import execute_from_command_line
